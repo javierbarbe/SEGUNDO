91dd3ebd299d5b3a7b933ed9dab19bc82dd8429b
javierbarbe <55160533+javierbarbe@users.noreply.github.com>
2020-12-18 18:15:44 +0100

    Add files via upload

---
+++ blazorUdemyTeoria/Shared/Entidades/Pelicula.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorUdemy.Shared.Entidades
+{
+   public class Pelicula
+    {
+        public string Imagen { get; set; }
+        public string Titulo { get; set; }
+        public DateTime Lanzamiento { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Pelicula pelicula &&
+                   Titulo == pelicula.Titulo &&
+                   Lanzamiento == pelicula.Lanzamiento;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Titulo, Lanzamiento);
+        }
+    }
+}
